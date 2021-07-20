e8f4e317d7a35bb227233884a4c43fedfc3f8df3
Justin Hui <huirayj@gmail.com>
2021-07-20 17:25:51 -0400

    started resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, { user }) => {
+            if (user) {
+                const user = await User.findOne({ _id: user._id });
+
+                return user;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook,
+        removeBook
+    }
+};
+
+module.exports = resolvers;
